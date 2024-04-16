198dc437763a88ebe708b355709e5a520a551929
SamChia <yongseng1688@gmail.com>
2024-04-16 18:12:01 +0800

    Just testing purpose

--- src/Create.js
+++ src/Create.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import { TextField } from '@material-ui/core';
-
+// Make me an 
 const Create = () => {
   return (
     <form className={classes.root} noValidate autoComplete="off">
-  <TextField id="standard-basic" label="Standard" />
+  <TextField id="standard-basic" label="Standarad" />
   <TextField id="filled-basic" label="Filled" variant="filled" />
   <TextField id="outlined-basic" label="Outlined" variant="outlined" />
 </form>
